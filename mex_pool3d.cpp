d5336a3697497a793f2e715ed840a96e1d7ee0d4
peng sun <pengsun000@gmail.com>
2015-05-08 17:16:09 -0400

    minor

---
+++ mex_pool3d.cpp
@@ -0,0 +1,8 @@
+#include "mex.h"
+
+void mexFunction(int no, mxArray       *vo[],
+                 int ni, mxArray const *vi[])
+{
+  
+  return;
+}
--- mex_pool3d.m
+++ mex_pool3d.m
@@ -1,16 +1,24 @@
 %MEX_POOL3D Volume max pooling for 3D convnet
-%  Y = mex_pool3d(X, pool); forward pass
-%  [dZdX] = mex_pool3d(X, pool, dZdY); backward pass
+%  [Y,ind] = MEX_POOL3D(X); forward pass
+%  dZdX = MEX_POOL3D(dZdY, ind); backward pass
+%  MEX_POOL3D(..., 'pool',pool, 'stride',s, 'pad',pad); options
 %
 %  Input:
-%   X: [H,W,D,M,N]. Input volume or feature maps. H, W, D are volume's 
+%   X: [H,W,D,M,N]. Volume at input port or feature maps. H, W, D are volume's 
 %   height, width and depth, respectively. M is #volumes (or #feature maps).
 %   N is #instances.
+%   dZdY: [Ho,Wo,Do,M,N]. Delta signal at output port. Z means loss.
+%   ind: see ind Output
+%
+% Options:  
 %   pool: [PH,PW,PD] or [P]. Pooling 3D window size. PH, PW, PD are the 
 %   height, width and depth, respectively. P is the size for all. Default 
-%   to 2.
+%   to P = 2.
+%   s: [sH,sW,SD] or [s]. Default to s = 2
+%   pad: [PH,PW,PD] or [P]. Padding size
 %
 %  Output:
-%   Y: [Ho,Wo,Do,M,N]. Output feature maps
-%   dZdX: [H,W,D,M,N]. Delta w.r.t X where Z means loss.  
+%   Y: [Ho,Wo,Do,M,N]. Feature maps at output port
+%   ind: [Ho,Wo,Do,M,N]. Linear index of the max elements to X: Y = X(ind)
+%   dZdX: [H,W,D,M,N]. Delta signal at input port. Z means loss.  
 %
