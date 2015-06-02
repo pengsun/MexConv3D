74460ba37762aba67c17441ebe7faad7b4daaa55
peng sun <pengsun000@gmail.com>
2015-06-02 13:14:06 -0400

    add scripts for timing mex_maxpool3d

--- make_all.m
+++ make_all.m
@@ -250,27 +250,27 @@ if (~exist(bld_dir,'dir')), mkdir(bld_dir); end
 srcs = horzcat(lib_src,mex_src) ;
 %%% compiling all with nvcc is slow but is necessary when 
 %%% debugging cuda code 
-for i = 1 : numel( srcs )
-  if strcmp(opts.cudaMethod,'nvcc')
-    nvcc_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.nvcc) ;
-  else
-    mex_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.mexcc) ;
-  end
-end
-
-%%% this code compiles faster, but cannot debug the cuda code 
 % for i = 1 : numel( srcs )
-%   [~,~,ext] = fileparts(srcs{i});
-%   if strcmp(ext, '.cu') 
-%     if strcmp(opts.cudaMethod,'nvcc')
-%       nvcc_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.nvcc) ;
-%     else
-%       mex_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.mexcc) ;
-%     end
+%   if strcmp(opts.cudaMethod,'nvcc')
+%     nvcc_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.nvcc) ;
 %   else
 %     mex_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.mexcc) ;
 %   end
 % end
+
+%%% this code compiles faster, but cannot debug the cuda code 
+for i = 1 : numel( srcs )
+  [~,~,ext] = fileparts(srcs{i});
+  if strcmp(ext, '.cu') 
+    if strcmp(opts.cudaMethod,'nvcc')
+      nvcc_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.nvcc) ;
+    else
+      mex_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.mexcc) ;
+    end
+  else
+    mex_compile(opts, srcs{i}, toobj(bld_dir,srcs{i}), flags.mexcc) ;
+  end
+end
 
 
 % Link into MEX files
